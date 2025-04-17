99c478a4e4ac07fb2fbaa8ee7a7cd8e1ccdee053
yejin-5112 <162818862+yejin-5112@users.noreply.github.com>
2025-04-17 15:05:47 +0900

    Add files via upload

---
+++ 250417/FifthActivity.java
@@ -0,0 +1,12 @@
+package com.example.nextpage;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FifthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth);
+    }
+}
---
+++ 250417/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.nextpage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button button = findViewById(R.id.button_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
